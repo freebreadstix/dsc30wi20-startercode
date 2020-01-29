75b971c06e6b37aff23f84a87c15d71aa2422a2a
Yuxuan Fan <39680169+991231@users.noreply.github.com>
2020-01-28 21:33:39 -0800

    PA04

---
+++ PA04/Deque.java
@@ -0,0 +1,49 @@
+public class Deque {
+
+    private DoublyLinkedList<Integer> list;
+
+    public Deque() throws IllegalArgumentException {
+        /* TODO */
+    }
+
+    public int size() {
+        /* TODO */
+        return 0;
+    }
+
+    public boolean isEmpty() {
+        /* TODO */
+        return false;
+    }
+
+    public boolean addFront(Integer i) throws NullPointerException {
+        /* TODO */
+        return false;
+    }
+
+    public boolean addBack(Integer i) throws NullPointerException {
+        /* TODO */
+        return false;
+    }
+
+    public Integer removeFront() {
+        /* TODO */
+        return null;
+    }
+
+    public Integer removeBack() {
+        /* TODO */
+        return null;
+    }
+
+    public Integer peekFront() {
+        /* TODO */
+        return null;
+    }
+
+    public Integer peekBack() {
+        /* TODO */
+        return null;
+    }
+
+}
